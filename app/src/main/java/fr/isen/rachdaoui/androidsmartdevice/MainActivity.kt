e2fa2e8fc6d98c90021c052c7ab5604c27f98a05
sofiane <sofiane.rachdaoui@isen.yncrea.fr>
2024-11-12 17:09:30 +0100

    projet init

--- app/src/main/java/fr/isen/rachdaoui/androidsmartdevice/MainActivity.kt
+++ app/src/main/java/fr/isen/rachdaoui/androidsmartdevice/MainActivity.kt
@@ -26,7 +26,8 @@ import androidx.compose.ui.tooling.preview.Preview
 
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override
+    fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidSmartDeviceTheme {
